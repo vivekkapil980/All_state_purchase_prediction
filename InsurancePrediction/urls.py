3a98c0d1eea08a75f1f21719510ca5fce5639466
V K <vivek980kapil@gmail.com>
2020-09-21 17:38:53 +0530

    tablefixed

--- InsurancePrediction/urls.py
+++ InsurancePrediction/urls.py
@@ -18,4 +18,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('insurance/',include('Training.urls')),
+    path('',include('Training.urls')),
 ]
--- Training/urls.py
+++ Training/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('main/', views.Home_Page,name="main_page"),
     path('test/', views.Test,name="test"),
     path('analysis/', views.pie_chart,name="analysis"),
+    path('', views.Test ,name="main")
 ]
